c55fd8ff0c196fa42dcaea31f403d01729a63228
Kr1ger <dragonil777@mail.ru>
2025-07-04 01:28:43 +0300

    Мониторинг GPU фейк режим

--- scripts/temp_contF.sh
+++ scripts/temp_contF.sh
@@ -2,7 +2,7 @@
 
 # Получение значений температуры
 #mapfile temp_gpu < <(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
-temp_gpu+=(45 44 39 47 43 43 42)
+temp_gpu+=(45 44 39 83 43 43 42)
 echo ${temp_gpu[@]}
 for  (( i = 0; i < "${#temp_gpu[@]}"; i++ ));do
  if [ "${temp_gpu[$i]}" -gt 82 ];then
