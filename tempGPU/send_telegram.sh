0551d1f8ca3b71bf06525cd609c8fbd7b5e74301
Kr1ger-r <dragonil777@mail.ru>
2025-07-10 13:43:38 +0300

    Update send_telegram.sh

--- tempGPU/send_telegram.sh
+++ tempGPU/send_telegram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BOT_TOKEN="8054698918:...."
-CHAT_ID="46774..."
+BOT_TOKEN="8025832918:...."
+CHAT_ID="467..."
 MESSAGE="$1"
 
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
